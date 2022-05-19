1295969e995fd3784708faee0f32b6e480b10a6f
b1mind <brent@1mindsolutions.com>
2022-05-19 15:37:47 -0600

    🧪testing bind target scroll with onDestroy

---
+++ src/lib/TriggerMe.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { gsap } from 'gsap'
+  import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.js'
+  import { onDestroy } from 'svelte'
+  import { onMount } from 'svelte'
+  gsap.registerPlugin(ScrollTrigger)
+
+  let target
+  const duration = 1
+
+  onMount(() => {
+    console.dir(target)
+    const tl = gsap.timeline({})
+    return tl.to(target, {
+      duration,
+      opacity: 1,
+      scrollTrigger: {
+        target: '.timeline',
+        scrub: 1,
+        markers: true,
+        start: 'center center',
+        end: '+=3000 center',
+      },
+    })
+  })
+
+  onDestroy(() => {
+    console.log('destroy or not to destroy that is the question')
+    ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+  })
+
+  // function timelineActive(node) {
+  //   tl.to(node, {
+  //     duration,
+  //     x: 300,
+  //     scrollTrigger: {
+  //       target: node,
+  //       pin: '.timeline',
+  //       scrub: 1,
+  //       markers: true,
+  //       start: 'center center',
+  //       end: '+=3000 center',
+  //     },
+  //   })
+  // }
+</script>
+
+<header class="timeline" bind:this={target}>
+  <div>
+    <slot />
+  </div>
+</header>
+
+<style lang="scss">
+</style>
