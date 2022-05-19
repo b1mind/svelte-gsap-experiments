632b9b46c4e1d3be481b6d8fc5922581b419a3d4
b1mind <brent@1mindsolutions.com>
2022-05-19 16:44:16 -0600

    🧪testing semi working triggers

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,12 @@
+<script context="module">
+  // import { page } from '$app/stores'
+  export const load = async ({ url }) => ({ props: { url } })
+</script>
+
 <script>
-  import { page } from '$app/stores'
   import Transition from '/src/lib/Transition.svelte'
+
+  export let url
 </script>
 
 <nav>
@@ -9,15 +15,28 @@
 </nav>
 
 <main>
-  <Transition urlProp={$page.url}>
+  <Transition urlProp={url}>
     <slot />
   </Transition>
 </main>
 
 <style>
+  :global(body) {
+    margin: 0;
+    height: 300vh;
+    background: #1b1b1b;
+  }
+
+  :global(p) {
+    margin: 0;
+  }
+
   main {
     position: relative;
+    min-height: 100vh;
     display: grid;
     grid-template-areas: 'l';
+    place-items: center;
+    overflow: hidden;
   }
 </style>
